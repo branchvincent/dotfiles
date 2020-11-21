4331b24a1f5e35d0ea9bc3281650f9d93aa674f9
Branch Vincent <branchevincent@gmail.com>
2020-11-21 13:09:27 -0500

    code: don't rename app

--- .config/yadm/bootstrap.d/02-macos.sh
+++ .config/yadm/bootstrap.d/02-macos.sh
@@ -24,7 +24,7 @@ dockutil --no-restart --add /System/Applications/Messages.app
 dockutil --no-restart --add /Applications/Slack.app
 dockutil --no-restart --add /Applications/Spotify.app
 dockutil --no-restart --add /Applications/Google\ Chrome.app
-dockutil --no-restart --add /Applications/VS\ Code.app
+dockutil --no-restart --add /Applications/Visual\ Studio\ Code.app
 dockutil --no-restart --add /Applications/iTerm.app
 dockutil --no-restart --add /System/Applications/System\ Preferences.app
 dockutil --no-restart --add ~/Documents --sort name --display folder --view list
