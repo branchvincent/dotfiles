b1233f282f51d6ff5d66985538078c773ab6c6e1
Branch Vincent <branchevincent@gmail.com>
2021-01-17 12:08:58 -0500

    code: add settings.json

--- .config/yadm/bootstrap.d/02-yadm.sh
+++ .config/yadm/bootstrap.d/02-yadm.sh
@@ -4,6 +4,3 @@
 
 yadm gitconfig status.showUntrackedFiles normal
 yadm sparse-checkout set '/*' '!/bootstrap'
-
-# Duplicate .gitignore for vscode: https://github.com/microsoft/vscode/issues/38878
-echo '{"files.exclude": {"{[^.]*,.cache,.local,.Trash,.vscode}": true}}' >~/.vscode/settings.json
