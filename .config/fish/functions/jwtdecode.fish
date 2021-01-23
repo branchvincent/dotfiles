29ed4f27529211dff837c1092a9a8e3213aead7b
Branch Vincent <branchevincent@gmail.com>
2021-01-23 13:07:43 -0500

    minor tweaks

--- .config/fish/functions/jwtdecode.fish
+++ .config/fish/functions/jwtdecode.fish
@@ -1,4 +1,4 @@
 function jwtdecode --description "Decode a JWT"
     isatty stdin || read -a argv
-    echo $argv | jq -R 'gsub("-";"+") | gsub("_";"/") | split(".") | .[1] | @base64d | fromjson'
+    echo $argv | jq -R 'gsub("-"; "+") | gsub("_"; "/") | split(".") | .[1] | @base64d | fromjson'
 end
