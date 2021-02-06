a5f592c52b9c934515222883069940fcd73e0dc9
Branch Vincent <branchevincent@gmail.com>
2021-02-06 11:18:03 -0500

    tide: add k8s

--- .config/fish/conf.d/tide.fish
+++ .config/fish/conf.d/tide.fish
@@ -2,17 +2,23 @@
 
 # Configure prompt items
 set -g tide_left_prompt_items pwd git newline prompt_char
-set -g tide_right_prompt_items status cmd_duration context jobs node python rust direnv
+set -g tide_right_prompt_items status cmd_duration context jobs k8s node python rust direnv
 
 # Define custom prompt items
 function _tide_item_direnv
     set -q DIRENV_DIR && echo (set_color yellow)▼
 end
 
+function _tide_item_k8s
+    set -l namespace /(kubens --current)
+    test $namespace = "/default" && set namespace ""
+    echo (set_color magenta)⎈ (kubectl config current-context)$namespace
+end
+
 function _tide_item_node
-    test -f package.json && echo (set_color 00AFAF)⬢ (node --version | string replace v '')
+    test -f package.json && echo (set_color 00AFAF)⬢ (node --version | string replace v "")
 end
 
 function _tide_item_python
-    set -q VIRTUAL_ENV && echo (set_color 00AFAF) (python --version | string replace 'Python ' '')
+    set -q VIRTUAL_ENV && echo (set_color 00AFAF) (python --version | string replace "Python " "")
 end
