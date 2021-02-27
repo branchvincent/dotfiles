67397161b32b4916da5fb9323417f01696808c99
Branch Vincent <branchevincent@gmail.com>
2021-02-27 13:16:04 -0500

    pipx: protect against venv breakage
    https://github.com/pipxproject/pipx/issues/633#issuecomment-782806161

--- .config/fish/conf.d/python.fish
+++ .config/fish/conf.d/python.fish
@@ -1,4 +1,5 @@
 set -x PIP_DISABLE_PIP_VERSION_CHECK 1
 set -x PIP_REQUIRE_VIRTUALENV 1
+set -x PIPX_DEFAULT_PYTHON /usr/local/bin/python3 # don't break venv's on brew upgrade
 set -x POETRY_VIRTUALENVS_IN_PROJECT true
 set -x PYTHONDONTWRITEBYTECODE 1
