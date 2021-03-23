1b8757420b3b96e137992a103fed4f872331f75e
Branch Vincent <branchevincent@gmail.com>
2021-03-22 21:50:11 -0400

    fish: export colors

--- .config/fish/conf.d/theme.fish
+++ .config/fish/conf.d/theme.fish
@@ -3,31 +3,31 @@
 # References:
 # - https://github.com/rkbk60/onedark-fish
 
-set -g fish_greeting
-set -g fish_color_autosuggestion 555 brblack
-set -g fish_color_cancel 555 brblack
-set -g fish_color_command brmagenta
-set -g fish_color_comment brblack
-set -g fish_color_cwd brred
-set -g fish_color_cwd_root red
-set -g fish_color_end white
-set -g fish_color_error brred
-set -g fish_color_escape brcyan
-set -g fish_color_history_current --bold
-set -g fish_color_host brgreen
-set -g fish_color_host_remote yellow
-set -g fish_color_match brcyan --underline
-set -g fish_color_normal white
-set -g fish_color_operator brmagenta
-set -g fish_color_param brcyan
-set -g fish_color_quote brgreen
-set -g fish_color_redirection brcyan
-set -g fish_color_search_match --background=blue
-set -g fish_color_selection --background=brblack
-set -g fish_color_status red
-set -g fish_color_user brblue
-set -g fish_color_valid_path brred --underline
-set -g fish_pager_color_completion white
-set -g fish_pager_color_description B3A06D yellow
-set -g fish_pager_color_prefix brgreen
-set -g fish_pager_color_progress white
+set -gx fish_greeting
+set -gx fish_color_autosuggestion 555 brblack
+set -gx fish_color_cancel 555 brblack
+set -gx fish_color_command brmagenta
+set -gx fish_color_comment brblack
+set -gx fish_color_cwd brred
+set -gx fish_color_cwd_root red
+set -gx fish_color_end white
+set -gx fish_color_error brred
+set -gx fish_color_escape brcyan
+set -gx fish_color_history_current --bold
+set -gx fish_color_host brgreen
+set -gx fish_color_host_remote yellow
+set -gx fish_color_match brcyan --underline
+set -gx fish_color_normal white
+set -gx fish_color_operator brmagenta
+set -gx fish_color_param brcyan
+set -gx fish_color_quote brgreen
+set -gx fish_color_redirection brcyan
+set -gx fish_color_search_match --background=blue
+set -gx fish_color_selection --background=brblack
+set -gx fish_color_status red
+set -gx fish_color_user brblue
+set -gx fish_color_valid_path brred --underline
+set -gx fish_pager_color_completion white
+set -gx fish_pager_color_description B3A06D yellow
+set -gx fish_pager_color_prefix brgreen
+set -gx fish_pager_color_progress white
