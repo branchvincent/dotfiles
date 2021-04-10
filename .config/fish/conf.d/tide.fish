9fb661db768f9524c76b28f4e302fc0b3ad3d96a
Branch Vincent <branchevincent@gmail.com>
2021-04-10 18:07:58 -0400

    tide: handle versions on stderr
    i.e. python2

--- .config/fish/conf.d/tide.fish
+++ .config/fish/conf.d/tide.fish
@@ -10,7 +10,7 @@ set -g tide_right_prompt_items status cmd_duration context jobs k8s go java node
 ### Custom items ###
 function _tide_language_version
     argparse --stop-nonopt "c/color=" "i/icon=" -- $argv || return
-    set -l v ($argv | string match -r "\d+\.\d+\.\d+")
+    set -l v ($argv 2>&1 | string match -r "\d+\.\d+\.\d+")
     set -q _flag_color[1] || set _flag_color 00AFAF
     echo (set_color $_flag_color)$_flag_icon $v
 end
