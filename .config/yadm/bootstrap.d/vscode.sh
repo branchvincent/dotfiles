4a33111927bcbcf21a5bcbeb8ab23f8c61934ae4
Branch Vincent <branchevincent@gmail.com>
2021-07-24 11:14:36 -0400

    code: force xdg

--- .config/yadm/bootstrap.d/vscode.sh
+++ .config/yadm/bootstrap.d/vscode.sh
@@ -1,6 +1,11 @@
 # shellcheck shell=bash
 #
-# Install vscode extensions
+# Configure vscode
 
+# Settings
+ln -sfn ~/.config/code/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -sfn ~/.config/code/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+
+# Extensions
 mkdir -p ~/Code
 sed 's/^/--install-extension /' ~/.config/code/extensions.txt | xargs code
