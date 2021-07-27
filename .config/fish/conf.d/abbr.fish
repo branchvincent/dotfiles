cc8acccbedda4c0ac10cdc44e087c3ac1817010d
Branch Vincent <branchevincent@gmail.com>
2021-07-27 12:25:06 -0400

    fish: remove old abbr
    superseded by `fish --no-config`

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -10,7 +10,6 @@ abbr -ag df "yadm enter code ~"
 abbr -ag dprune "docker system prune --all --volumes --force"
 abbr -ag drun "docker run --rm -it"
 abbr -ag dstop "docker ps --quiet | xargs docker stop"
-abbr -ag fish_clean 'sh -c \'env HOME=$(mktemp -d) fish\''
 abbr -ag g git
 abbr -ag h helm
 abbr -ag k kubectl
