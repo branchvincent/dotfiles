d2ab2cdc7be31804368183f01724cdbb4610f6f7
Branch Vincent <branchevincent@gmail.com>
2021-07-26 22:53:51 -0400

    tweak

--- .config/fish/conf.d/fisher.fish
+++ .config/fish/conf.d/fisher.fish
@@ -2,8 +2,8 @@
 
 # Isolate fisher-managed packages
 set -g fisher_path $XDG_DATA_HOME/fish/vendor
-set -p fish_function_path $fish_function_path[1] $fisher_path/functions && set -e fish_function_path[3]
-set -p fish_complete_path $fish_complete_path[1] $fisher_path/completions && set -e fish_complete_path[3]
+set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
+set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
 
 # Source conf.d snippets
 for file in $fisher_path/conf.d/*.fish
