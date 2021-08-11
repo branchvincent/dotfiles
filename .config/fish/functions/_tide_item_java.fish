4434caef89fb624263c2611f5d871755e3d4c8fd
Branch Vincent <branchevincent@gmail.com>
2021-08-11 18:39:56 -0400

    tide: update java color

--- .config/fish/functions/_tide_item_java.fish
+++ .config/fish/functions/_tide_item_java.fish
@@ -1,5 +1,5 @@
 function _tide_item_java --description 'Show Java version'
     if set -q tide_java_always_display || test -f pom.xml -o -f build.gradle -o -f build.gradle.kts
-        _tide_language_version -i -cED8B00 java -Xinternalversion
+        _tide_language_version -i -c5382A1 java -Xinternalversion
     end
 end
