353fe44610649f23da8a7a9f0e7c29737d7b8c8b
Branch Vincent <branchevincent@gmail.com>
2021-08-13 22:07:23 -0400

    tide: update direnv item

--- .config/fish/functions/_tide_item_direnv.fish
+++ .config/fish/functions/_tide_item_direnv.fish
@@ -1,7 +1,6 @@
 function _tide_item_direnv --description 'Show direnv status'
     set -q DIRENV_DIR || return
-    set -l color yellow
-    direnv status | string match -rq '^Found RC allowed false$' && set color brred
-    tide_direnv_color=$color tide_direnv_bg_color=normal \
-        _tide_print_item direnv ▼
+    set -q tide_direnv_color_denied && direnv status | string match -rq '^Found RC allowed false$' \
+        && set -lx tide_direnv_color $tide_direnv_color_denied
+    _tide_print_item direnv ▼
 end
