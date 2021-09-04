0075bc4ec9e4c2b39da77ae78373dd416e707355
Branch Vincent <branchevincent@gmail.com>
2021-09-04 13:43:26 -0400

    fish: fix pager for diff-so-fancy

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,6 @@ status is-login || exit
 
 set -gx EDITOR code --wait
 set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
-set -gx PAGER less -RF --tabs=4
+set -gx PAGER less --tabs=4 -RF -+X
 set -Uq fish_features || set -U fish_features 3.{0,1,4}
 fish_add_path -gmP $XDG_BIN_DIRS /usr/local/{,s}bin
