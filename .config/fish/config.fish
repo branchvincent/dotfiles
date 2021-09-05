4393052516283e93f1c7909bea2987ddb7b02d74
Branch Vincent <branchevincent@gmail.com>
2021-09-05 09:14:50 -0700

    fish: enable all features

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,5 +3,5 @@ status is-login || exit
 set -gx EDITOR code --wait
 set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
 set -gx PAGER less --tabs=4 -RF -+X
-set -Uq fish_features || set -U fish_features 3.{0,1,4}
+set -Uq fish_features || set -U fish_features all
 fish_add_path -gmP $XDG_BIN_DIRS /usr/local/{,s}bin
