bcb6433104577c1be206f6874f71f361109e579b
Branch Vincent <branchevincent@gmail.com>
2021-10-03 22:24:40 -0400

    brew: symlink taps

--- .config/yadm/bootstrap.d/brew.sh
+++ .config/yadm/bootstrap.d/brew.sh
@@ -2,7 +2,7 @@
 #
 # Install Homebrew and packages
 
-has brew || echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+has brew || NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew bundle install --file=~/.config/brew/Brewfile
 
 # Don't require sudo for tlmgr
@@ -13,3 +13,9 @@ xattr -dr com.apple.quarantine ~/Library/QuickLook/
 
 # Symlink default java
 sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+
+# Symlink repos for development
+mkdir -p ~/Code/Homebrew
+ln -sfn "$(brew --repo branchvincent/tap)" ~/Code/Homebrew/homebrew-tap
+ln -sfn "$(brew --repo homebrew/core)" ~/Code/Homebrew/homebrew-core
+ln -sfn "$(brew --repo homebrew/cask)" ~/Code/Homebrew/homebrew-cask
--- .config/yadm/bootstrap.d/vscode.sh
+++ .config/yadm/bootstrap.d/vscode.sh
@@ -7,5 +7,4 @@ ln -sfn ~/.config/code/settings.json ~/Library/Application\ Support/Code/User/se
 ln -sfn ~/.config/code/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 
 # Extensions
-mkdir -p ~/Code
 sed 's/^/--install-extension /' ~/.config/code/extensions.txt | xargs code
