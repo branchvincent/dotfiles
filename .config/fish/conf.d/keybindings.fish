8a7eec7099de9a7df99f74730a173b2d982140ac
Branch Vincent <branchevincent@gmail.com>
2022-02-17 22:51:58 -0500

    fish: add keybinding to toggle yadm

--- .config/fish/conf.d/keybindings.fish
+++ .config/fish/conf.d/keybindings.fish
@@ -3,3 +3,10 @@
 status is-interactive || exit
 
 bind \cc 'commandline ""' # Ctrl-C -> clear line
+bind \cP '
+if not set -e GIT_DIR GIT_WORK_TREE
+    set -gx GIT_DIR "$XDG_DATA_HOME/yadm/repo.git"
+    set -gx GIT_WORK_TREE ~
+end
+commandline -f repaint
+'
