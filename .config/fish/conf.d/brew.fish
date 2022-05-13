ceaa4813404041bca1f08dd5d8d601aa05b7c35a
Branch Vincent <branchevincent@gmail.com>
2022-05-13 10:03:56 -0400

    fish: handle brew not in PATH

--- .config/fish/conf.d/brew.fish
+++ .config/fish/conf.d/brew.fish
@@ -2,8 +2,18 @@ set -gx HOMEBREW_BAT 1
 set -gx HOMEBREW_NO_ENV_HINTS 1
 
 # Faster `brew shellenv`
-set -q HOMEBREW_PREFIX || set -U HOMEBREW_PREFIX (brew --prefix)
+if not set -q HOMEBREW_PREFIX
+    switch "$(uname -sm)"
+        case "Darwin arm64"
+            set -U HOMEBREW_PREFIX /opt/homebrew
+        case "Linux x86_64"
+            set -U HOMEBREW_PREFIX /home/linuxbrew/.linuxbrew
+        case '*'
+            set -U HOMEBREW_PREFIX /usr/local
+    end
+end
 set -gx HOMEBREW_CELLAR $HOMEBREW_PREFIX/Cellar
 set -gx HOMEBREW_REPOSITORY $HOMEBREW_PREFIX/Homebrew
 set -gxp MANPATH $HOMEBREW_PREFIX/share/man
 set -gxp INFOPATH $HOMEBREW_PREFIX/share/info
+fish_add_path -gmP $XDG_BIN_DIRS $HOMEBREW_PREFIX/{,s}bin
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,4 +4,3 @@ set -gx EDITOR code --wait
 set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
 set -gx PAGER less --tabs=4 -RF -+X
 set -Uq fish_features || set -U fish_features all
-fish_add_path -gmP $XDG_BIN_DIRS $HOMEBREW_PREFIX/{,s}bin
