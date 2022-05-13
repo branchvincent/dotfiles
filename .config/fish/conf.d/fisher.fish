b3d9b284e6963a44029729fcea6bda045205c727
Branch Vincent <branchevincent@gmail.com>
2022-05-13 13:06:12 -0400

    fish: move fisher bootstrap

--- .config/fish/conf.d/fisher.fish
+++ .config/fish/conf.d/fisher.fish
@@ -2,8 +2,15 @@
 
 # Isolate fisher-managed packages
 set -g fisher_path $XDG_DATA_HOME/fish/vendor
-set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
-set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
+set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..]
+set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..]
+
+# Install
+if not functions -q fisher && status is-login
+    curl -sL git.io/fisher | source
+    fisher install <~/.config/fish/fish_plugins >/dev/null
+    printf 1112111y | COLUMNS=55 LINES=21 tide configure
+end
 
 # Source conf.d snippets
 for file in $fisher_path/conf.d/*.fish
--- .config/yadm/bootstrap.d/shell.sh
+++ .config/yadm/bootstrap.d/shell.sh
@@ -8,12 +8,3 @@ if [ "$SHELL" != "$shell" ]; then
     echo "$shell" | sudo tee -a /etc/shells
     chsh -s "$shell"
 fi
-
-# Install fisher/plugins
-fish -c '
-if not functions -q fisher
-    curl -sL git.io/fisher | source
-    fisher install <~/.config/fish/fish_plugins >/dev/null
-    printf 1112111y | tide configure >/dev/null
-end
-'
