6dd95ea66c9e94767d40f8d01637469d7d51176c
Branch Vincent <branchevincent@gmail.com>
2022-05-21 17:26:42 -0400

    tide: tweak missing venv

--- .config/fish/functions/_tide_item_python.fish
+++ .config/fish/functions/_tide_item_python.fish
@@ -1,6 +1,6 @@
 function _tide_item_python --description 'Show Python version'
     if set -q tide_python_always_display || test -n "$VIRTUAL_ENV" -o -f pyproject.toml -o -f setup.py
-        test -n "$VIRTUAL_ENV" && set color 4584B6 || set color red
+        test -n "$VIRTUAL_ENV" -o -d __pypackages__ && set color 4584B6 || set color red
         _tide_language_version -i -c$color python3 --version
     end
 end
