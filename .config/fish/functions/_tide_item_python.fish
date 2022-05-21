ead3a12b144f02a1326997c560184fc48e8f13c6
Branch Vincent <branchevincent@gmail.com>
2022-05-21 17:06:18 -0400

    tide: indicate missing venv

--- .config/fish/functions/_tide_item_python.fish
+++ .config/fish/functions/_tide_item_python.fish
@@ -1,5 +1,6 @@
 function _tide_item_python --description 'Show Python version'
     if set -q tide_python_always_display || test -n "$VIRTUAL_ENV" -o -f pyproject.toml -o -f setup.py
-        _tide_language_version -i -c4584B6 python3 --version
+        test -n "$VIRTUAL_ENV" && set color 4584B6 || set color red
+        _tide_language_version -i -c$color python3 --version
     end
 end
