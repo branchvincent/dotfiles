303602e1eab4b6cebdb0085cb7bf01f9ff4b7628
Branch Vincent <branchevincent@gmail.com>
2022-05-21 18:41:01 -0400

    fish: trim trailing newline for `sha256`

--- .config/fish/functions/sha256.fish
+++ .config/fish/functions/sha256.fish
@@ -1,3 +1,3 @@
 function sha256 -a url --description "Compute SHA-256 for given url"
-    curl -fsSL $url | shasum -a 256 | string split -f1 " " | pbcopy
+    curl -fsSL $url | shasum -a 256 | string split -f1 " " | string collect | pbcopy
 end
