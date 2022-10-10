b5aef0216b59528fca1420cc3812d8090a535e6f
Branch Vincent <branchevincent@gmail.com>
2022-10-09 23:08:45 -0400

    fish: add more keybindings

--- .config/fish/conf.d/keybindings.fish
+++ .config/fish/conf.d/keybindings.fish
@@ -2,9 +2,11 @@
 
 status is-interactive || exit
 
+bind \cc 'commandline ""'
+bind \a 'gh repo view --web &>/dev/null'
 bind \cO workon
-bind \cc 'commandline ""' # Ctrl-C -> clear line
-bind \cP '
+bind \cP 'workon --exec cd; commandline -f repaint'
+bind \cU '
 if not set -e GIT_DIR GIT_WORK_TREE
     set -gx GIT_DIR "$XDG_DATA_HOME/yadm/repo.git"
     set -gx GIT_WORK_TREE ~
--- .config/fish/functions/workon.fish
+++ .config/fish/functions/workon.fish
@@ -1,17 +1,27 @@
 function workon --description "Open a project"
+    argparse --stop-nonopt exec= -- $argv || return
+    if set -q _flag_exec
+        set cmd $_flag_exec
+    else
+        set cmd code
+    end
+
     set taps (string replace $HOMEBREW_PREFIX/Library/Taps/ ./  $HOMEBREW_PREFIX/Library/Taps/*/*)
     set choices ./homebrew/brew $taps (git workspace list)
     set chosen (string split ' ' $choices | sort -u | fzf -q "$argv")
+    set -e GIT_DIR GIT_WORK_TREE
     switch "$chosen"
         case ./branchvincent/dotfiles
-            yadm enter code ~
+            set -gx GIT_DIR "$XDG_DATA_HOME/yadm/repo.git"
+            set -gx GIT_WORK_TREE ~
+            $cmd ~
         case ./homebrew/brew
-            env -u GIT_DIR -u GIT_WORK_TREE code $HOMEBREW_REPOSITORY
+            $cmd $HOMEBREW_REPOSITORY
         case $taps
-            env -u GIT_DIR -u GIT_WORK_TREE code $HOMEBREW_PREFIX/Library/Taps/$chosen
+            $cmd $HOMEBREW_PREFIX/Library/Taps/$chosen
         case ''
             return
         case \*
-            env -u GIT_DIR -u GIT_WORK_TREE code $GIT_WORKSPACE/$chosen
+            $cmd $GIT_WORKSPACE/$chosen
     end
 end
