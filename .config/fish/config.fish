346ea29e046c5b90ef85fa5a5396af5c43750508
Branch Vincent <branchevincent@gmail.com>
2022-10-31 09:25:28 -0400

    fish: fix manpager formatting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 status is-login || exit
 
 set -gx EDITOR code --wait
-set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
+set -gx MANPAGER "MANROFFOPT=-c sh -c 'col -bx | bat -l man -p'"
 set -gx PAGER less --tabs=4 -RF -+X
 set -Uq fish_features || set -U fish_features all
