258888c9d2d308d2dd624becd6915922ac1464e1
Branch Vincent <branchevincent@gmail.com>
2022-11-01 23:32:52 -0400

    fisher: tweak aws completions

--- .config/fish/completions/aws.fish
+++ .config/fish/completions/aws.fish
@@ -1,3 +1,3 @@
 if type -q aws_completer
-    complete aws -fa '(COMP_LINE=(commandline -pc) aws_completer)'
+    complete aws -fa '(COMP_LINE=(commandline -pc) COMP_POINT=(commandline -C) aws_completer)'
 end
