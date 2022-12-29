6328687ab333c5da942cf6e823de79ca38b06a35
Branch Vincent <branchevincent@gmail.com>
2022-12-28 23:51:43 -0800

    fish: register completions for non-brewed fish

--- .config/fish/conf.d/brew.fish
+++ .config/fish/conf.d/brew.fish
@@ -20,6 +20,14 @@ set -gxp MANPATH $HOMEBREW_PREFIX/share/man
 set -gxp INFOPATH $HOMEBREW_PREFIX/share/info
 status is-login && fish_add_path -gmP $XDG_BIN_DIRS $HOMEBREW_PREFIX/{,s}bin
 
+# Register completions (for non-brewed fish)
+# https://docs.brew.sh/Shell-Completion#configuring-completions-in-fish
+set -l comp_dir $HOMEBREW_PREFIX/share/fish/vendor_completions.d
+if not contains $comp_dir $fish_complete_path
+    set -l idx (contains -i $__fish_data_dir/completions $fish_complete_path)
+    set fish_complete_path $fish_complete_path[..(math $idx - 1)] $comp_dir $fish_complete_path[$idx..]
+end
+
 # Tell build tools about our special prefix
 # https://docs.brew.sh/Homebrew-and-Python#brewed-python-modules
 if test $HOMEBREW_PREFIX != /usr/local
