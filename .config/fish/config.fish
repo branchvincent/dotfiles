17bcd7110364c4973223f5594a1c4072c36755fb
Branch Vincent <branchevincent@gmail.com>
2023-01-05 23:29:05 -0800

    git: replace diff-so-fancy with delta

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,7 @@ status is-login || exit
 
 set -gx EDITOR code --wait
 set -gx MANPAGER "MANROFFOPT=-c sh -c 'col -bx | bat -l man -p'"
-set -gx PAGER less --tabs=4 -RF -+X
+set -gx LESS --ignore-case --Raw-control-chars --quit-if-one-screen
+set -gx PAGER less
+type -q delta || set -gx GIT_PAGER $PAGER
 set -Uq fish_features || set -U fish_features all
