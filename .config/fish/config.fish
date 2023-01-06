e0bf9a8075ba9934f8a27d293b48199bbf8b84c1
Branch Vincent <branchevincent@gmail.com>
2023-01-06 00:04:41 -0800

    fish: update editor/pager

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,17 @@
 status is-login || exit
 
-set -gx EDITOR code --wait
-set -gx MANPAGER "MANROFFOPT=-c sh -c 'col -bx | bat -l man -p'"
+set -Uq fish_features || set -U fish_features all
+
+# Editor
+set -gx EDITOR vim
+if type -q code
+    set -gx EDITOR code --wait
+else if type -q helix
+    set -gx EDITOR helix
+end
+
+# Pagers
 set -gx LESS --ignore-case --Raw-control-chars --quit-if-one-screen
 set -gx PAGER less
+type -q bat && set -gx MANPAGER "MANROFFOPT=-c sh -c 'col -bx | bat -l man -p'"
 type -q delta || set -gx GIT_PAGER $PAGER
-set -Uq fish_features || set -U fish_features all
