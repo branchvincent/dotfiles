8ab3464a846dc937e04706f04336f7d587234552
Branch Vincent <branchevincent@gmail.com>
2023-01-07 02:39:32 -0800

    brew: prune packages

--- .config/fish/functions/_tide_item_java.fish
+++
@@ -1,5 +0,0 @@
-function _tide_item_java --description 'Show Java version'
-    if set -q tide_java_always_display || test -f pom.xml -o -f build.gradle -o -f build.gradle.kts
-        _tide_language_version -i -c5382A1 java -Xinternalversion
-    end
-end
--- .config/fish/functions/gradle.fish
+++
@@ -1,7 +0,0 @@
-function gradle --description "Gradle wrapper to prefer gradlew"
-    if test -x gradlew
-        ./gradlew $argv
-    else
-        command gradle $argv
-    end
-end
