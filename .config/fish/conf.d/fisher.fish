3c683563d6595823f33014564515b554532cf93a
Branch Vincent <branchevincent@gmail.com>
2023-01-16 17:14:31 -0800

    fisher: update fisher_path

--- .config/fish/conf.d/fisher.fish
+++ .config/fish/conf.d/fisher.fish
@@ -1,9 +1,9 @@
 # Fisher: https://github.com/jorgebucaran/fisher
 
 # Isolate fisher-managed packages
-set -g fisher_path $XDG_DATA_HOME/fish/vendor
-set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..]
+set -g fisher_path $XDG_DATA_HOME/fisher
 set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..]
+set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..]
 
 # Install
 if not functions -q fisher && status is-login
