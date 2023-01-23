04c0e4e6b20d288d606b8a35b82101e6c960288e
Branch Vincent <branchevincent@gmail.com>
2023-01-22 22:00:35 -0800

    yadm: move yadm config to hooks

--- .config/yadm/bootstrap.d/yadm.sh
+++
@@ -1,7 +0,0 @@
-# shellcheck shell=bash
-#
-# Configure yadm
-
-debug "Updating settings"
-yadm gitconfig status.showUntrackedFiles normal
-(cd ~ && yadm sparse-checkout set --no-cone '/*' '!/bootstrap')
