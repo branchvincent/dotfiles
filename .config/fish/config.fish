534759161f5d42b347238725033600dcb583b344
Branch Vincent <branchevincent@gmail.com>
2023-02-13 22:49:51 -0800

    fish: set man colors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,21 +1,20 @@
 status is-login || exit
 
-set -Uq fish_features || set -U fish_features all
-command -q docker || functions -e docker
-
 # Environment
 set -gx BAT_THEME TwoDark
-set -gx EDITOR vim
-if command -q code
-    set -gx EDITOR code --wait
-else if command -q hx
-    set -gx EDITOR hx
-end
+set -gx EDITOR hx
 set -gx GIT_MERGE_AUTOEDIT no # accept default merge commit message
 set -gx GIT_WORKSPACE ~/Code
 set -gx LESS --ignore-case --Raw-control-chars --quit-if-one-screen
+set -gx man_bold --bold $fish_color_command
+set -gx man_standout --reverse F0CB02
+set -gx man_underline --underline $fish_color_param
 set -gx PAGER less
+
+set -Uq fish_features || set -U fish_features all
+string match -q "$TERM_PROGRAM" vscode && set -gx EDITOR code --wait
 command -q delta || set -gx GIT_PAGER $PAGER
+command -q docker || functions -e docker
 
 # Daily update
 up --auto
