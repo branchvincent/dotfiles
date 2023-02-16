1724440b1d98392a71d3414c22dc2937464c0f85
Branch Vincent <branchevincent@gmail.com>
2023-02-15 22:01:51 -0800

    yadm: update brew

--- .config/yadm/bootstrap.d/brew.sh
+++ .config/yadm/bootstrap.d/brew.sh
@@ -2,18 +2,11 @@
 #
 # Install Homebrew and packages
 
-export PATH="/opt/homebrew/bin:/usr/local/bin${PATH+:$PATH}"
+export PATH="/opt/homebrew/bin:/usr/local/bin:/home/linuxbrew/.linuxbrew/bin${PATH+:$PATH}"
 if ! has brew; then
     debug "Installing Homebrew"
     NONINTERACTIVE=1 HOMEBREW_INSTALL_FROM_API=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
-
-# Ensure `brew` is in PATH
-case "$(uname -sm)" in
-"Darwin arm64") eval "$(/opt/homebrew/bin/brew shellenv)" ;;
-"Darwin x86_64") eval "$(/usr/local/bin/brew shellenv)" ;;
-"Linux x86_64") eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" ;;
-esac
 
 debug "Checking Homebrew packages"
 export HOMEBREW_BUNDLE_FILE=~/.config/brew/Brewfile
