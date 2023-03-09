73ec598c03918f9543270a76221eb5237e5614d5
Branch Vincent <branchevincent@gmail.com>
2023-03-08 20:17:41 -0800

    yadm: merge package script

--- .config/yadm/bootstrap.d/brew.sh
+++ .config/yadm/bootstrap.d/brew.sh
@@ -2,6 +2,7 @@
 #
 # Install Homebrew and packages
 
+### brew ###
 export PATH="/opt/homebrew/bin:/usr/local/bin:/home/linuxbrew/.linuxbrew/bin${PATH+:$PATH}"
 if ! has brew; then
     debug "Installing Homebrew"
@@ -13,4 +14,20 @@ export HOMEBREW_BUNDLE_FILE=~/.config/brew/Brewfile
 if ! has yadm || ! brew bundle check &>/dev/null; then
     debug "Installing Homebrew packages"
     brew bundle install --no-lock
+fi
+
+### rust ###
+if ! has rustup; then
+    debug "Installing Rust"
+    export CARGO_HOME=~/.local/share/cargo RUSTUP_HOME=~/.local/share/rustup
+    curl -fsS https://sh.rustup.rs | sh -s -- -y --quiet --no-modify-path
+fi
+
+### vscode ###
+if has code; then
+    missing=$(comm -13 <(code --list-extensions | sort) <(sort ~/.config/code/extensions.txt))
+    if [ -n "$missing" ]; then
+        debug "Installing VSCode extensions"
+        echo "$missing" | sed 's/^/--install-extension /' | xargs code
+    fi
 fi
