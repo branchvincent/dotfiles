58696fe045d8d90d05c758cdae6b04430ddf9054
Branch Vincent <branchevincent@gmail.com>
2023-03-23 19:19:12 -0700

    yadm: silence `tee`

--- .config/yadm/bootstrap.d/shell.sh
+++ .config/yadm/bootstrap.d/shell.sh
@@ -6,6 +6,6 @@
 shell=$(command -v fish)
 if ! grep -q "$shell" /etc/shells; then
     debug "Updating login shell"
-    echo "$shell" | sudo tee -a /etc/shells
+    echo "$shell" | sudo tee -a /etc/shells >/dev/null
     chsh -s "$shell"
 fi
