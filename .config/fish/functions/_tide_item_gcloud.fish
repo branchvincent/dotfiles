932f0fba0d71e9d1c644070679baea19fae4adb9
Branch Vincent <branchevincent@gmail.com>
2023-03-31 20:06:31 -0700

    tide: update gcloud icon

--- .config/fish/functions/_tide_item_gcloud.fish
+++ .config/fish/functions/_tide_item_gcloud.fish
@@ -7,5 +7,5 @@ function _tide_item_gcloud --description "Show Google cloud project"
     # Get active project
     path is $CLOUDSDK_CONFIG/configurations/config_$config || return
     string match -rg '^project = (.*)' <$CLOUDSDK_CONFIG/configurations/config_$config | read -l project &&
-        _tide_print_item gcloud ' ' $project
+        _tide_print_item gcloud $tide_gcloud_icon' ' $project
 end
