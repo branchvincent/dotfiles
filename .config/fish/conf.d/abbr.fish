d2f1003667d67dded842941802fb0ea980465dd6
Branch Vincent <branchevincent@gmail.com>
2023-09-01 16:24:31 -0700

    fish: add podman alias

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -26,6 +26,7 @@ function _alias
     command -q $argv[2] && alias $argv
 end
 _alias cat bat
+_alias docker podman
 if command -q lsd
     alias ls "lsd --icon=never"
     alias ll "ls -l --header --date=relative --blocks=permission,size,user,date,name"
