9236e5268c520ce9b3f6b97c28fca3938772ab1b
Branch Vincent <branchevincent@gmail.com>
2024-08-03 10:46:17 -0700

    gpip: add --break-system-packages

--- .config/fish/functions/gpip.fish
+++ .config/fish/functions/gpip.fish
@@ -1,3 +1,3 @@
 function gpip --wraps pip3 --description "Global pip"
-    PIP_REQUIRE_VIRTUALENV=0 $HOMEBREW_PREFIX/bin/pip3 $argv
+    PIP_REQUIRE_VIRTUALENV=0 PIP_BREAK_SYSTEM_PACKAGES=1 "$HOMEBREW_PREFIX"/bin/pip3 $argv
 end
