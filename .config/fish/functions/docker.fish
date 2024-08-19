a4371db8949009bdf0bae6c9734e66b31fee369e
Branch Vincent <branchevincent@gmail.com>
2024-08-18 20:47:07 -0700

    fish: handle linux in docker wrapper

--- .config/fish/functions/docker.fish
+++ .config/fish/functions/docker.fish
@@ -1,5 +1,5 @@
 function docker --description "Docker wrapper to ensure the daemon is running"
-    if ! pgrep -q OrbStack
+    if test -d /Applications/OrbStack.app && ! pgrep -q OrbStack
         # Boot daemon
         open -jga OrbStack || exit 1
         while ! curl -f --unix-socket /var/run/docker.sock localhost/_ping &>/dev/null
