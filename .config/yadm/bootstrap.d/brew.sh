3179f07a221de63a4d4da26342b706ea8589b48f
Branch Vincent <branchevincent@gmail.com>
2024-09-07 20:37:13 -0700

    ci: test on alpine

--- .config/yadm/bootstrap.d/brew.sh
+++ .config/yadm/bootstrap.d/brew.sh
@@ -2,6 +2,11 @@
 #
 # Install Homebrew and packages
 
+if [ -n "${CI:-}" ]; then
+    debugw "Skipping due to \$CI"
+    return
+fi
+
 export PATH="/opt/homebrew/bin:/usr/local/bin:/home/linuxbrew/.linuxbrew/bin${PATH+:$PATH}"
 if ! has brew; then
     debug "Installing Homebrew"
@@ -10,9 +15,7 @@ fi
 
 debug "Installing Homebrew packages"
 export HOMEBREW_BUNDLE_FILE=~/.config/brew/Brewfile
-if [ -n "${CI:-}" ]; then
-    debugw "Skipping due to \$CI"
-elif ! has yadm || ! brew bundle check &>/dev/null; then
+if ! has yadm || ! brew bundle check &>/dev/null; then
     brew bundle install --no-lock
     ! has rustup || RUSTUP_HOME=~/.local/share/rustup rustup default stable
 fi
