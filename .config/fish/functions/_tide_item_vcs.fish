51678f3779f538c2c416c8e287ca4273645054d1
Branch Vincent <branchevincent@gmail.com>
2025-10-31 12:27:53 -0700

    tide: fix uncolored branch

--- .config/fish/functions/_tide_item_vcs.fish
+++ .config/fish/functions/_tide_item_vcs.fish
@@ -1,4 +1,7 @@
 function _tide_item_vcs
+    # HACK: tide special cases the `git` item
+    # https://github.com/IlanCosman/tide/blob/main/functions/_tide_cache_variables.fish
+    set_color $tide_git_color_branch | read -gx _tide_location_color
     if path is -d $_tide_parent_dirs/.hg
         _tide_item_hg
     else if path is -d $_tide_parent_dirs/.jj
