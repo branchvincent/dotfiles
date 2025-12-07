8378704241a906c4d70541cdaa65098f62b70639
Branch Vincent <branchevincent@gmail.com>
2025-12-07 14:02:52 -0800

    yadm: more accurate chsh

--- .config/yadm/bootstrap.d/shell.sh##c.Home,e.sh
+++ .config/yadm/bootstrap.d/shell.sh##c.Home,e.sh
@@ -1,13 +1,23 @@
 # shellcheck shell=bash
 #
-# Configure fish shell
-
 # Update default login shell
+
 fish=$(command -v fish || true)
 if ! [ -x "$fish" ]; then
     debugw "Skipping due to missing fish"
-elif ! grep -q "$fish" /etc/shells; then
-    debug "Updating login shell"
-    echo "$fish" | sudo tee -a /etc/shells >/dev/null
-    chsh -s "$fish"
+    return
 fi
+
+if [ "$(uname)" = Darwin ]; then
+    shell=$(dscl . -read ~/ UserShell | sed 's/UserShell: //')
+else
+    shell=$(getent passwd "$(whoami)" | cut -d: -f7)
+fi
+if [ "$shell" = "$fish" ]; then
+    debugw "Login shell is already $shell"
+    return
+fi
+
+debug "Updating login shell: $shell -> $fish"
+grep -q "$fish" /etc/shells || echo "$fish" | sudo tee -a /etc/shells >/dev/null
+chsh -s "$fish"
